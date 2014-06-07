4c0af6eac5772e7919034e79ef9f4999c7ea3a6f
Marius Cojocariu <marius@nyxpoint.com>
2014-06-07 16:05:21 +0300

    Create mixpanel.js
    generic mixpanel.js that tracks close, purchase, variation click events and remembers the first clicked variation as the "preferred" one

---
+++ mixpanel.js
@@ -0,0 +1,58 @@
+(function($) {
+	"use strict";
+	
+	function trackVariationInterest() {
+		$('a[data-mixpanel-variation]').on('click', function(e) {
+			var $this = $(this);
+
+			mixpanel.register_once({
+		        "prefered variation name": $this.attr('data-mixpanel-variation')
+		    });
+			mixpanel.track("variation click", {
+				"referrer": document.referrer, 
+				"variation name": $this.attr('data-mixpanel-variation')
+			}, function() {
+				window.location = $this.attr('href')
+			});
+
+			e.preventDefault();
+			return false;
+		});
+		$('a[data-mixpanel-purchase]').on('click', function(e) {
+			var $this = $(this);
+
+			mixpanel.track("purchase", $.extend(
+				{
+					"referrer": document.referrer, 
+				}, $this.attr('data-mixpanel-variation') ? {
+					"variation name": $this.attr('data-mixpanel-variation')
+				} : {}
+			), function() {
+				window.location = $this.attr('href')
+			});
+
+			e.preventDefault();
+			return false;
+		});
+		$('a[data-mixpanel-close]').on('click', function(e) {
+			var $this = $(this);
+
+			mixpanel.track("close", $.extend(
+				{
+					"referrer": document.referrer, 
+				}, $this.attr('data-mixpanel-variation') ? {
+					"variation name": $this.attr('data-mixpanel-variation')
+				} : {}
+			), function() {
+				window.location = $this.attr('href')
+			});
+
+			e.preventDefault();
+			return false;
+		});
+	}
+
+	$(function() {
+		trackVariationInterest();
+	});
+})(jQuery);
