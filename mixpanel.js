865a2f095611c44fe71ae8d4d205240b9533fd14
marius-nyxpoint <marius@nyxpoint.com>
2014-06-09 13:54:42 +0300

    fix issue with close and purchase triggering a variation event

--- mixpanel.js
+++ mixpanel.js
@@ -2,22 +2,25 @@
 	"use strict";
 	
 	function trackVariationInterest() {
-		$('a[data-mixpanel-variation]').on('click', function(e) {
-			var $this = $(this);
+		$('a[data-mixpanel-variation]')
+			.not('a[data-mixpanel-purchase]')
+			.not('a[data-mixpanel-close]')
+			.on('click', function(e) {
+				var $this = $(this);
 
-			mixpanel.register_once({
-		        "prefered variation name": $this.attr('data-mixpanel-variation')
-		    });
-			mixpanel.track("variation click", {
-				"referrer": document.referrer, 
-				"variation name": $this.attr('data-mixpanel-variation')
-			}, function() {
-				window.top.location = $this.attr('href')
+				mixpanel.register_once({
+			        "prefered variation name": $this.attr('data-mixpanel-variation')
+			    });
+				mixpanel.track("variation click", {
+					"referrer": document.referrer, 
+					"variation name": $this.attr('data-mixpanel-variation')
+				}, function() {
+					window.top.location = $this.attr('href')
+				});
+
+				e.preventDefault();
+				return false;
 			});
-
-			e.preventDefault();
-			return false;
-		});
 		$('a[data-mixpanel-purchase]').on('click', function(e) {
 			var $this = $(this);
 
