2479153e90e621a90653d1cda2dd61cd3b5c0c8a
marius-nyxpoint <marius@nyxpoint.com>
2014-06-11 19:11:41 +0300

    implemented landing page event

--- mixpanel.js
+++ mixpanel.js
@@ -1,61 +1,106 @@
 (function($) {
 	"use strict";
+
+	var trackingEnabled = false, 
+		trackingDeferred = $.Deferred();
 	
 	function trackVariationInterest() {
 		$('a[data-mixpanel-variation]')
 			.not('a[data-mixpanel-purchase]')
 			.not('a[data-mixpanel-close]')
 			.on('click', function(e) {
-				var $this = $(this);
+				var $this = $(this), 
+					track = function() {
+						mixpanel.register_once({
+					        "prefered variation name": $this.attr('data-mixpanel-variation')
+					    });
+						mixpanel.track("variation click", {
+							"referrer": document.referrer, 
+							"variation name": $this.attr('data-mixpanel-variation')
+						}, function() {
+							window.top.location = $this.attr('href')
+						});
+					};
 
-				mixpanel.register_once({
-			        "prefered variation name": $this.attr('data-mixpanel-variation')
-			    });
-				mixpanel.track("variation click", {
-					"referrer": document.referrer, 
-					"variation name": $this.attr('data-mixpanel-variation')
-				}, function() {
-					window.top.location = $this.attr('href')
-				});
+				if(trackingEnabled) {
+					track();
+				} else {
+					trackingDeferred.done(function() {
+						track();
+					});
+				}
 
 				e.preventDefault();
 				return false;
 			});
 		$('a[data-mixpanel-purchase]').on('click', function(e) {
-			var $this = $(this);
+			var $this = $(this), 
+				track = function() {
+					mixpanel.track("purchase", $.extend(
+						{
+							"referrer": document.referrer, 
+						}, $this.attr('data-mixpanel-variation') ? {
+							"variation name": $this.attr('data-mixpanel-variation')
+						} : {}
+					), function() {
+						window.top.location = $this.attr('href')
+					});
+				};
 
-			mixpanel.track("purchase", $.extend(
-				{
-					"referrer": document.referrer, 
-				}, $this.attr('data-mixpanel-variation') ? {
-					"variation name": $this.attr('data-mixpanel-variation')
-				} : {}
-			), function() {
-				window.top.location = $this.attr('href')
-			});
+			if(trackingEnabled) {
+				track();
+			} else {
+				trackingDeferred.done(function() {
+					track();
+				});
+			}
 
 			e.preventDefault();
 			return false;
 		});
 		$('a[data-mixpanel-close]').on('click', function(e) {
-			var $this = $(this);
+			var $this = $(this), 
+				track = function() {
+					mixpanel.track("close", $.extend(
+						{
+							"referrer": document.referrer, 
+						}, $this.attr('data-mixpanel-variation') ? {
+							"variation name": $this.attr('data-mixpanel-variation')
+						} : {}
+					), function() {
+						window.top.location = $this.attr('href')
+					});
+				};
 
-			mixpanel.track("close", $.extend(
-				{
-					"referrer": document.referrer, 
-				}, $this.attr('data-mixpanel-variation') ? {
-					"variation name": $this.attr('data-mixpanel-variation')
-				} : {}
-			), function() {
-				window.top.location = $this.attr('href')
-			});
+			if(trackingEnabled) {
+				track();
+			} else {
+				trackingDeferred.done(function() {
+					track();
+				});
+			}
 
 			e.preventDefault();
 			return false;
 		});
 	}
 
-	$(function() {
-		trackVariationInterest();
-	});
+	if(!$('div[data-mixpanel-landing]').length) {
+		$(function() {
+			trackingEnabled = true;
+			trackVariationInterest();
+		});
+	} else {
+		$(function() {
+			trackVariationInterest();
+		});
+		$(window).load(function() {
+			mixpanel.track("landing page loaded", {
+				"referrer": document.referrer
+			}, function() {
+				trackingDeferred.resolve();
+				trackingEnabled = true;
+			});
+		});
+	}
 })(jQuery);
